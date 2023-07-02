a586b5c59d202be39aca2c4f604cac4bd6838e03
Jakub517 <pavlu.jakub8@seznam.cz>
2023-07-02 13:11:38 +0200

    nově funguje i na mobilních zařízeních (beta 1.1)

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 window.addEventListener('DOMContentLoaded', function() {
-    var zoomContainer = document.querySelector('.zoom-container');
-    zoomContainer.classList.add('zoom-animation');
-  });
+  var zoomContainer = document.querySelector('.zoom-container');
+  zoomContainer.classList.add('zoom-animation');
+});
 
-  
-  
-  // Získání všech tlačítek
+
+
 var buttons = document.getElementsByClassName("button");
 
-// Přidání události mouseover a mouseout na každé tlačítko
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("mouseover", function() {
-    this.querySelector("img").style.transform = "scale(1.2)";
-  });
+buttons[i].addEventListener("mouseover", function() {
+  this.querySelector("img").style.transform = "scale(1.2)";
+});
 
-  buttons[i].addEventListener("mouseout", function() {
-    this.querySelector("img").style.transform = "scale(1)";
-  });
+buttons[i].addEventListener("mouseout", function() {
+  this.querySelector("img").style.transform = "scale(1)";
+});
 
-  buttons[i].addEventListener("click", function() {
-    var link = this.querySelector("a").getAttribute("href");
-    window.location.href = link;
-  });
+buttons[i].addEventListener("click", function() {
+  var link = this.querySelector("a").getAttribute("href");
+  window.location.href = link;
+});
 }
 
+const toggleButton = document.getElementsByClassName('odkazove-tlacitko')[0]
+const navbarLinks = document.getElementsByClassName('navigace-link')[0]
+
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active')
+})
